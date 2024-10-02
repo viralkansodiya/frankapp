659faa1ba8501a7ec15afecaff52ff9d3a1e2597
viralpatel15 <viralkansodiya167@gmail.com>
2024-10-02 23:51:19 +0000

    ch

--- frankapp/hooks.py
+++ frankapp/hooks.py
@@ -108,9 +108,9 @@ app_license = "MIT"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Currency Exchange Settings": "frankapp.frankapp.api.vestaCurrencyExchangeSettings"
+}
 
 # Document Events
 # ---------------
--- frankapp/update_currency_exchange_settings_for_frankfurter.py
+++ frankapp/update_currency_exchange_settings_for_frankfurter.py
@@ -9,33 +9,3 @@ def execute():
 	set_parameters_and_result(doc)
 	doc.flags.ignore_validate = True
 	doc.save()
-
-
-def set_parameters_and_result(doc):
-	if doc.service_provider == "exchangerate.host":
-		if not doc.access_key:
-			frappe.throw(
-				_("Access Key is required for Service Provider: {0}").format(
-					frappe.bold(doc.service_provider)
-				)
-			)
-
-		doc.set("result_key", [])
-		doc.set("req_params", [])
-
-		doc.api_endpoint = get_api_endpoint(doc.service_provider, doc.use_http)
-		doc.append("result_key", {"key": "result"})
-		doc.append("req_params", {"key": "access_key", "value": doc.access_key})
-		doc.append("req_params", {"key": "amount", "value": "1"})
-		doc.append("req_params", {"key": "date", "value": "{transaction_date}"})
-		doc.append("req_params", {"key": "from", "value": "{from_currency}"})
-		doc.append("req_params", {"key": "to", "value": "{to_currency}"})
-	elif doc.service_provider == "frankfurter.app":
-		doc.set("result_key", [])
-		doc.set("req_params", [])
-
-		doc.api_endpoint = get_api_endpoint(doc.service_provider, doc.use_http)
-		doc.append("result_key", {"key": "rates"})
-		doc.append("result_key", {"key": "{to_currency}"})
-		doc.append("req_params", {"key": "base", "value": "{from_currency}"})
-		doc.append("req_params", {"key": "symbols", "value": "{to_currency}"})
